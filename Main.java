83537c578566e345f106de6242a087dfafc08f52
Atishay Gwari <83054615+atishay-gwari@users.noreply.github.com>
2021-12-15 15:34:05 +0530

    Add files via upload
    Java  basic theory.

---
+++ Main.java
@@ -0,0 +1,138 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Single Line Comment.
+        /*
+         * Double Line Comment
+         * Hii This first program
+         * */
+
+        // Print Simply your name
+//        System.out.println("Hello MPSTME");
+//        System.out.println("Hello Hii");
+
+        // Variables
+//         int a = 10;
+//         String hm = "Hello";
+//        System.out.println(a);
+
+        // Data Types
+//        1. Primitive Data Type
+/*         * 1. byte - 1 bytes - -128 to 127
+           * 2. short - 2 bytes - +- 32K
+           * 3. int - 4 bytes - +- 2B
+           * 4. long - 8 bytes - +- 9223372036854775808
+           * 5. float - 4 bytes -  Range (upto 7 digits)
+           * 6. double - 8 bytes - Range (upto 16 decimal digits)
+           * 7. char - 2bytes - A,B,C @,#
+           * 8. bool - 1 bytes -  True/False
+         * */
+        // Non- Primitive Data Types
+        // String , Arrays
+//        String hm = new String("Hello WOrld");
+//        String hg = "Hello WOrld";
+
+        // Arrays
+//         int[] marks = new int[10];
+//         marks[0]=45;
+//         marks[1]=65;
+//        int[] marks = {95,65,23};
+//        int[][] mark = {{1,2,3},{4,5,6}};
+//        System.out.println(mark[0][2]);
+//        System.out.println(marks[0]);
+//        System.out.println(marks[3]);
+//        System.out.println(marks);   
+
+        // Strings Methods (Immutable)
+        // Concatenation
+//        String name1 = new String("hello");
+//        String name2 = new String("World");
+//        String sentance = name1 + " " + name2 ;
+//        System.out.println(sentance);
+//
+//        // CharAt
+//        System.out.println(name1.charAt(1));
+//
+//        // Length
+//        System.out.println(name1.length());
+//
+//        // Substring
+//        System.out.println(name1.substring(0,2));
+
+        // Casting
+
+//        //1. Implicit Casting
+//        float price = 100.0F;
+//        int gst = 18;
+//        float finalprice = price + gst;
+//        System.out.println(finalprice);
+//        //2. Explicit Casting
+//        int prices = 100;
+//        float gsst = 18.0F;
+//        int finalPrice = prices + (int)gsst;
+//        System.out.println(finalPrice);
+
+        // COnstants
+//        final float PI = 3.14F;
+
+        // Operators
+        //1. ARithmetic Operators 2. Assignment 3. Comparison 4. Unary Operators(Increment/Decerement)
+//        int num = 10;
+////        num++ ==> num = num +1
+//        System.out.println(num++);
+//        System.out.println(num);
+
+
+        //Taking Input
+//        Scanner hm = new Scanner(System.in);
+//        System.out.println("Enter some Value");
+////        int n = hm.nextInt();
+////        String name = hm.next();
+//        String fullName = hm.nextLine();
+////        System.out.println(n);
+//        System.out.println(fullName);
+
+        // Decision Making
+
+//        int age = 30;
+//        if (age>10){
+//            System.out.println("This is Adult");
+//        } else {
+//            System.out.println("This is not adult");
+//        }
+
+//        if (age==10){
+//            System.out.println("This is Adult");
+//        } else if(age<20 )
+//        {
+//            System.out.println("This is not adult");
+//        } else {
+//            System.out.println("Budda Marr Gaya");
+//        }
+
+        // Switch Case
+//        int age = 2;
+//        switch(age){
+//            case 1:
+//                System.out.println("Adult");
+//                break;
+//            case 2:
+//                System.out.println("Not Adult");
+//                break;
+//            default:
+//                System.out.println("Not a Person");
+//        }
+//
+//Assignment:-
+//        1. Neon Number --> 9 -- 9*9 = 81 --> 8+1 = 9
+//        2. Armstrong Number --> 125 = 1^3 +2^3 +5 ^3 = 125
+//        3. ISBN Number -->   123456789 (10 digit)  = 9*1 + 8*2 + 7*3 + === SUm Value
+        /// Sum Value Divide 11 == Remainder is zero
+        // Initial number is ISBN
+
+    }
+}
