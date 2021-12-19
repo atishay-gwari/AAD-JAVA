72f8e9a4ddd735bd54c3957ba3ee3ff791da69db
Atishay Gwari <83054615+atishay-gwari@users.noreply.github.com>
2021-12-19 09:49:23 +0530

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -1,22 +1,28 @@
 package com.company;
 import java.util.*;
 public class Armstrong {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.print("Enter your number :");//ask the user to input an integer value
-        int n=sc.nextInt();
-        int x=n;
-        int sum=0;
-        while(x!=0)
+        System.out.print("Enter a number:");
+        int n = sc.nextInt();
+        int x;
+        int digit;
+        int sum = 0;
+        int c = 0;
+        x= n;
+        for (;x != 0; x/= 10)
         {
-            int digit= x%10;//divides the number in digits
-            x=x/10;
-            sum = sum +digit*digit*digit;// Adds the digits
+            c++;
         }
-        if (n==sum)//checks if the calculated sum is equal to the original number
-                    System.out.println(n+" is an armstrong number");
+        x=n;
+        for (;x!= 0; x /= 10)
+        {
+            digit = x % 10;
+            sum += Math.pow(digit, c);
+        }
+        if(sum == n)
+            System.out.println(n + " is an armstrong number");
         else
-                    System.out.println(n+" is not an armstrong number");
+            System.out.println(n + " is not an armstrong number");
     }
 }
